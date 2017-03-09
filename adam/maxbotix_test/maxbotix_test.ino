f14ba9d85f34ca0318ae6933aadfe7b2ce3fd955
David Sutherin <sutherin.3@wright.edu>
2017-03-09 11:48:16 -0500

    Adds MaxBotix sensor test

---
+++ adam/maxbotix_test/maxbotix_test.ino
@@ -0,0 +1,33 @@
+/*
+Test code for the Arduino Uno
+Written by Tom Bonar for testing
+Sensors being used for this code are the MB70X0 from MaxBotix
+*/
+const int pwPin1 = 2;
+
+long sensor1, cm;
+
+void setup () {
+  Serial.begin(9600);
+  pinMode(pwPin1, INPUT);
+}
+
+void read_sensor(){
+  sensor1 = pulseIn(pwPin1, HIGH);
+  cm = sensor1/58;
+}
+
+void loop () {
+  read_sensor();
+  printall();
+  delay(100);
+}
+
+void printall(){
+  Serial.print("Time: ");
+  Serial.print(millis());
+  Serial.print("\tS1: ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+}
