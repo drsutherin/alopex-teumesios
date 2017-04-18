a9c73c0f0e7800b3b0cd07fc52ee4833123452aa
David Sutherin <sutherin.3@wright.edu>
2017-04-18 14:43:50 -0400

    Updates ultrawave_2test; ready for testing

--- adam/testing/ultrawave_2test/ultrawave_2test.ino
+++ adam/testing/ultrawave_2test/ultrawave_2test.ino
@@ -3,6 +3,11 @@
  * simultaneously, using an object's measured distance and speed
  * to predict its future location (i.e. /at the next reading)
  * 
+ * Since the HB100 sensors aren't addressed, their power pins are 
+ * connected to the Arduino's digital ports, and HIGH is written to
+ * the pins when they're to be read from.  The sensors' data output
+ * pins are connected via a bus into Arduino pin 8
+ * 
  * @author  David Sutherin (sutherin.3@wright.edu)
  * @date    4/15/2017
  */
@@ -15,6 +20,8 @@ int i;
 double prediction;
 
 //Microwave/speed params
+int m1_pin = 0;
+int m2_pin = 1;
 #define NUM_READS 10
 float spd1;
 float spd2;
@@ -23,8 +30,7 @@ double sum2 = 0;
 
 // Ultrasonic/distance params
 #define SENSOR_1_ADDR 112
-#define SENSOR_2_ADDR 115
-int i = 0;
+#define SENSOR_2_ADDR 113
 int reading1 = 0;
 int reading2 = 0;
 
@@ -32,6 +38,8 @@ void setup() {
   Serial.begin(57600);
   Wire.begin();
   FreqMeasure.begin();
+  pinMode(m1_pin,OUTPUT);
+  pinMode(m2_pin,OUTPUT);
   Serial.println("Alopex Teumesios ADAM Test");
 }
 
@@ -41,21 +49,26 @@ void loop() {
   // Start ultrasonic sensor reading
   Wire.beginTransmission(SENSOR_1_ADDR);
   Wire.write(byte(0x51));
-  byte error = Wire.endTransmission();  
-  Serial.println("DEBUG: Ultrasonic 1 start");
+  byte error1 = Wire.endTransmission();  
 
   Wire.beginTransmission(SENSOR_2_ADDR);
   Wire.write(byte(0x51));
-  byte error = Wire.endTransmission();  
-  Serial.println("DEBUG: Ultrasonic 2 start");
+  byte error2 = Wire.endTransmission();
   
   // Get NUM_READS readings from microwave sensors
   while (i < NUM_READS) {
-    //Serial.print("DEBUG: i = "); Serial.println(i);
     if (FreqMeasure.available()) {
-      //Serial.println("DEBUG: Available");
-      sum = sum + FreqMeasure.read();
-      delay(50);
+      // Read from microwave sensor #1
+      digitalWrite(m1_pin,HIGH);
+      sum1 = sum1 + FreqMeasure.read();
+      digitalWrite(m1_pin,LOW);
+      
+      // Read from microwave sensor #2
+      digitalWrite(m2_pin,HIGH);
+      sum2 = sum2 + FreqMeasure.read();
+      digitalWrite(m2_pin,LOW);
+      delay(50);\
+      
       // Only increment if reading was successful
       // Note: could cause problems w/ prediction if readings take too long, consider 'for' loop instead
       i++;
@@ -63,33 +76,57 @@ void loop() {
   }
 
   // Use average of microwave sensor readings to calculate speed
-  float frequency = FreqMeasure.countToFrequency(sum/NUM_READS);
-  spd = frequency * 1.425;  // result in cm/s
-  Serial.print("Speed: ");
-  Serial.print(spd);
+  float frequency1 = FreqMeasure.countToFrequency(sum1/NUM_READS);
+  spd1 = frequency1 * 1.425;  // result in cm/s
+  Serial.print("SENSOR 1\tSpeed: ");
+  Serial.print(spd1);
   Serial.print("cm/s\t");
-  sum = 0;
+  sum1 = 0;
+
+  float frequency2 = FreqMeasure.countToFrequency(sum2/NUM_READS);
+  spd2 = frequency2 * 1.425;  // result in cm/s
+  Serial.print("SENSOR 2\tSpeed: ");
+  Serial.print(spd2);
+  Serial.println("cm/s\t");
+  sum2 = 0;
   
-  // Request reading from ultrasonic sensor
-  Wire.requestFrom(SENSOR_ADDR, 2);
+  // Request reading from ultrasonic sensor #1
+  Wire.requestFrom(SENSOR_1_ADDR, 2);
 
   // Receive reading from sensor
   if (2 <= Wire.available()) {
-    reading = Wire.read();      // receive high byte (overwrites previous reading)
-    reading = reading << 8;     // shift high byte to be high 8 bits
-    reading |= Wire.read();     // receive low byte as lower 8 bits
-    Serial.print("Range: ");
-    Serial.print(reading);   // print the reading
+    reading1 = Wire.read();      // receive high byte (overwrites previous reading)
+    reading1 = reading1 << 8;     // shift high byte to be high 8 bits
+    reading1 |= Wire.read();     // receive low byte as lower 8 bits
+    Serial.print("SENSOR 1\tRange: ");
+    Serial.print(reading1);   // print the reading
     Serial.print(" cm\t");
   }
-  else {
-    Serial.println("DEBUG: Wire not available");
+
+  // Request reading from ultrasonic sensor #2
+  Wire.requestFrom(SENSOR_2_ADDR, 2);
+
+  // Receive reading from sensor
+  if (2 <= Wire.available()) {
+    reading2 = Wire.read();      // receive high byte (overwrites previous reading)
+    reading2 = reading2 << 8;     // shift high byte to be high 8 bits
+    reading2 |= Wire.read();     // receive low byte as lower 8 bits
+    Serial.print("SENSOR 2\tRange: ");
+    Serial.print(reading2);   // print the reading
+    Serial.println(" cm\t");
   }
   
   // Calculate prediction for location @ next reading
-  Serial.print("PREDICTED LOCATION: ");
+  Serial.print("SENSOR 1\tPREDICTED LOCATION: ");
   // prediction = cm; reading = cm; .25 = sec; spd = cm/s
-  prediction = reading - (.25 * spd);
+  prediction = reading1 - (.25 * spd1);
   Serial.print(prediction);
   Serial.println("cm");
+
+  Serial.print("SENSOR 2\tPREDICTED LOCATION: ");
+  // prediction = cm; reading = cm; .25 = sec; spd = cm/s
+  prediction = reading2 - (.25 * spd2);
+  Serial.print(prediction);
+  Serial.println("cm");
+  Serial.println();
 }
